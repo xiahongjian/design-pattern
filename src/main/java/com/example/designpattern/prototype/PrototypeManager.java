4f3bf9e55a7a44f18a83e24d0a4c5eae67a435dd
xiahongjian <hongjian.xia@qq.com>
2018-01-26 14:09:56 +0800

    add comments

--- src/main/java/com/example/designpattern/prototype/PrototypeManager.java
+++ src/main/java/com/example/designpattern/prototype/PrototypeManager.java
@@ -4,10 +4,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 原型管理器
+ * <p>原型管理器</p>
  * 
- * 保存了一些列类的原型，当需要某个类的实例时，就使用对应类的原型对象进行克隆并返回
+ * <p>保存了一些列类的原型，当需要某个类的实例时，就使用对应类的原型对象进行克隆并返回
+ * 目前的FAR,SRS等类clone实现都是浅拷贝，深拷贝可以使用ObjectStream将对象序列化，
+ * 然后再反序列化得到新的对象</p>
+ * <pre>
+ * ByteArrayOutputStream bao = new ByteArrayOutputStream();
+ * ObjectOutputStream out = new ObjectOutputStream(bao);
+ * out.writeObject(this);
  * 
+ * ByteArrayInputStream bai = new ByteArrayInputStrea(bao.toByteArray());
+ * ObjectInputStream in -= new ObjectInputStream(bai);
+ * in.reateObject();
+ * </pre>
  * @author xiahongjian
  * @time   2018:01:26 10:40:54
  */
